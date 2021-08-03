1fbb6904904b567cf06cb167ec3e9952f9cf4220
Gagandeep Uppal <guppal.profile@gmail.com>
2021-08-03 14:07:29 +0000

    iterating over array

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+//while loop
+let i = 0;
+while(i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+
+
+//for loop
+for(let i = 0; i < ingredients.length; i++){
+console.log(ingredients[i]);
+}
+
+//for loop backward printing in array
+
+for (let i = ingredients.length - 1; i >= 0; i--){
+  console.log(ingredients[i]);
+}
